fffda47fdaaa011d84f648913652e4da4b2785f5
Saikiran Belana <88756154+belanasaikiran@users.noreply.github.com>
2022-11-16 11:02:14 +0530

    create bmw gltf project

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import "./App.css";
+
+import {Canvas} from "@react-three/fiber"
+import {useGLTF, Stage, PresentationControls} from "@react-three/drei"
+
+
+function Model(props){
+  const {scene} = useGLTF("/bmw.glb");
+  return <primitive object={scene} {...props} />
+}
+
+
+function App() {
+  return (
+   <Canvas dpr={[1,2]} shadows camera={{fov:45}} style={{"position": "absolute"}}>
+
+    <color attach="background" args={["#101010"]} />
+    <PresentationControls speed={1.5} global zoom={.5} polar={[-0.1, Math.PI/4]}>
+    <Stage environment={null}>
+
+      <Model scale={0.1} />
+    </Stage>
+
+    </PresentationControls>
+   </Canvas>  
+  );
+}
+
+export default App;
